6780f8bb428142eba3cb69d75eb944be7e5ed1f1
Your Name <you@example.com>
2025-02-08 12:54:12 +0530

    first commit

--- keys.js
+++ keys.js
@@ -1,10 +1,6 @@
-// module.exports ={
-//     MONGO_URI:"mongodb+srv://mahen:12345a@cluster0.qfgbe.mongodb.net/E-voting?retryWrites=true&w=majority",
-//     JWT_SECRET:"MOHENMONDALJDFKFLL"
+require("dotenv").config(); // Load environment variables from .env file
 
-// }
-module.exports ={
-    MONGO_URI:"mongodb+srv://jayadeeproyal8:ckzLH7oPpca1igoR@cluster0.gk0vagf.mongodb.net",
-    JWT_SECRET:"MOHENMONDALJDFKFLL"
-
-}
+module.exports = {
+    MONGO_URI: process.env.MONGO_URI,  // Fetch from environment variable
+    JWT_SECRET: process.env.JWT_SECRET, // Fetch from environment variable
+};
